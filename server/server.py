14b9c0b21e6f00546f7bd16701a1e1cc938ab34b
Felix Bade <flix.bade@gmail.com>
2022-02-12 18:56:40 +0200

    Add broadcast websocket server

---
+++ server/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import struct
+import asyncio
+import threading
+
+from websockets import serve
+
+connections = []
+
+async def echo(websocket):
+    print('New connection')
+
+    global connections
+    connections.append(websocket)
+    
+    async for message in websocket:
+        print(message)
+        await asyncio.gather(*[c.send(message) for c in connections])
+
+    print('Connection ended')
+
+async def main():
+    async with serve(echo, "localhost", 8000):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
